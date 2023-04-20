dd60983d6b3787b55e63272d17f14db0afa256b1
chikov <dmitrychickov@gmail.com>
2023-04-20 23:38:35 +0300

    feat(server) проверка уникальности пользователя перед созданием по email

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { User, UserDocument } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/user.dto';
 
@@ -8,7 +8,13 @@ import { CreateUserDto } from './dto/user.dto';
 export class UserService {
   @InjectModel(User.name) private model: Model<UserDocument>;
 
-  create(data: CreateUserDto) {
+  async findUserByEmail(email: string){
+    return this.model.findOne({email})
+  }
+
+  async create(data: CreateUserDto) {
+    const candidate = await this.findUserByEmail(data.email)
+    if(candidate) throw new BadRequestException(`Пользователь c email ${data.email} уже существует`)
     return this.model.create(data);
   }
 
