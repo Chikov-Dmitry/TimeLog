e90db4aee1c800bedf1eb5cc8626e497c47e018c
chikov <dmitrychickov@gmail.com>
2023-06-02 11:59:43 +0300

    feat(client) basic template of main page

---
+++ apps/client/src/components/TimerBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <span v-if="!isStarted">Запустить таймер</span>
+  <span v-else>Остановить таймер</span>
+  <p-button @click="btnClick" class="timer-btn" :icon="btnIcon" style="font-size: 3rem; width: 60px; height: 60px"/>
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from "vue";
+
+const isStarted = ref(false)
+
+const btnIcon = computed(()=>{
+  return isStarted.value ? "mdi mdi-stop" : "mdi mdi-play"
+})
+
+const btnClick = function () {
+  isStarted.value = !isStarted.value
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/variables.scss";
+
+.timer-btn{
+  border-radius: $borderRadius;
+}
+</style>
--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,11 +1,73 @@
 <template>
   <div>
-    home
-    <p-button @click="router.push({ name: 'signIn' })" />
+    <div class="grid mb-5">
+      <div class="col-12 sm:col-6 md:col-4">
+        <div class="card h-full flex flex-column justify-content-center today-card">
+          <div class="text-3xl font-bold">Сегодня</div>
+          <div class="text-xl font-semibold">Пон 22, 2021 | 10:00</div>
+        </div>
+      </div>
+      <div class="col-12 sm:col-6 md:col-4 md:col-offset-4">
+        <div class="card h-full flex align-items-center  justify-content-around text-xl font-semibold">
+          <timer-btn/>
+        </div>
+      </div>
+    </div>
+    <div class="grid stat-block">
+      <div class="col-12 md:col-4">
+        <div class="card  h-full flex flex-column justify-content-center ">
+          <div class="text-2xl font-semibold">За месяц</div>
+          <div class="flex align-items-center justify-content-between">
+            <div class="text-4xl font-bold text-orange-500">160:05:32</div>
+            <div class="flex align-items-center justify-content-center border-round"
+                 style="width: 6.5rem; height: 6.5rem">
+              <i class="mdi mdi-calendar-month-outline text-primary" style="font-size: 6rem"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 md:col-4">
+        <div class="card  h-full flex flex-column justify-content-center ">
+          <div class="text-2xl font-semibold">За неделю</div>
+          <div class="flex align-items-center justify-content-between">
+            <div class="text-4xl font-bold text-green-500">40:24:32</div>
+            <div class="flex align-items-center justify-content-center border-round"
+                 style="width: 6.5rem; height: 6.5rem">
+              <i class="mdi mdi-calendar-week-outline text-primary" style="font-size: 6rem"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 md:col-4">
+        <div class="card  h-full flex flex-column justify-content-center ">
+          <div class="text-2xl font-semibold">Сегодня</div>
+          <div class="flex align-items-center justify-content-between">
+            <div class="text-4xl font-bold text-primary">8:43:32</div>
+            <div class="flex align-items-center justify-content-center border-round"
+                 style="width: 6.5rem; height: 6.5rem">
+              <i class="mdi mdi-calendar-today-outline text-primary" style="font-size: 6rem"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import {useRouter} from 'vue-router'
+import TimerBtn from "@/components/TimerBtn.vue";
+
 const router = useRouter()
 </script>
+
+<style scoped lang="scss">
+@media (max-width: 576px) {
+  .today-card{
+    align-items: center;
+  }
+  .stat-block{
+    flex-direction: column-reverse;
+  }
+}
+</style>
