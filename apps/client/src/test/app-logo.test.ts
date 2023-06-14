76cce43b92877460bbbd915d1c34e52065f7b26d
chikov <dmitrychickov@gmail.com>
2023-06-14 14:57:04 +0300

    test(client) add test for app-logo

---
+++ apps/client/src/test/app-logo.test.ts
@@ -0,0 +1,17 @@
+import {mount} from "@vue/test-utils";
+import AppLogo from "@/components/AppLogo.vue";
+import {test, expect} from "vitest";
+
+
+test(`app-logo component`, async ()=>{
+    expect(AppLogo).toBeTruthy()
+
+
+    const component = mount(AppLogo, {
+        props:{
+            fontSize: "14px"
+        }
+    })
+
+    expect(component.text()).toContain('T ime L og')
+})
